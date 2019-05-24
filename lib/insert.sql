049670e0376f4d95affbd8980bffee513e113f8c
Jim Hough <teachlife@outlook.com>
2019-05-24 02:07:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Black', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'Male', 'Black', 'Happy', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 6, 'Female', 'Brown', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 4, 'Male', 'Green', 'Angry', true);
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Mr. Chocolate', 3, 'Male', 'Black', 'Angry');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Rowdy', 3, 'Male', 'Black', 'Happy');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Tabitha', 6, 'Female', 'Brown', 'Angry');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Sergeant Brown', 4, 'Male', 'Green', 'Angry');
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 8, 'Female', 'Black', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'Black', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 5, 'Female', 'Black', 'Angry', true);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, 'Male', 'Black', 'Angry', true);
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Melissa', 8, 'Female', 'Black', 'Angry');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Grinch', 7, 'Male', 'Black', 'Angry');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Wendy', 5, 'Female', 'Black', 'Angry');
+INSERT INTO bears (name, age, gender, color, temperament) VALUES (NULL, NULL, 'Male', 'Black', 'Angry');
