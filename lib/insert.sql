b5206d3b908a5aa5ea649cf4b6ba4bd93bd12796
Jim Hough <teachlife@outlook.com>
2019-05-24 02:08:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,15 @@
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Mr. Chocolate', 3, 'Male', 'Black', 'Angry');
+
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Rowdy', 3, 'Male', 'Black', 'Happy');
-INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Tabitha', 6, 'Female', 'Brown', 'Angry');
+
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Tabitha', 6, 'Female', 'Brown', 'Goofy');
+
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Sergeant Brown', 4, 'Male', 'Green', 'Angry');
 
 INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Melissa', 8, 'Female', 'Black', 'Angry');
-INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Grinch', 7, 'Male', 'Black', 'Angry');
-INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Wendy', 5, 'Female', 'Black', 'Angry');
+
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Grinch', 7, 'Male', 'Black', 'Goofy');
+
+INSERT INTO bears (name, age, gender, color, temperament) VALUES ('Wendy', 5, 'Female', 'Black', 'Happy');
+
 INSERT INTO bears (name, age, gender, color, temperament) VALUES (NULL, NULL, 'Male', 'Black', 'Angry');
